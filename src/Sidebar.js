382d7b0a3a1f19a5f2a1b60888e5209d99bce8e9
Verdilac <lathindu2000@gmail.com>
2020-09-07 00:19:04 +0530

    searchbar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import React from 'react';
 import './Sidebar.css';
 import  DonutLargeIcon  from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
     return (
@@ -11,14 +12,29 @@ function Sidebar() {
         <div className='sidebar__header'>
             <Avatar />
         <div className ='sidebar__headerRight'>
-            <DonutLargeIcon /> 
-            <ChatIcon />
-            <MoreVertIcon />
+            <IconButton>
+                <DonutLargeIcon />
+            </IconButton>
+            
+            <IconButton>
+                <ChatIcon />
+            </IconButton>
+            
+            <IconButton>
+                <MoreVertIcon />
+            </IconButton>
+            
+            
         </div>
 
         </div>
 
         <div className='sidebar__search'>
+            
+            <div class='sidebar__searchContainer'>
+            <SearchOutlined/>
+            <input placeholder='Search or Start New Chat' type = 'text'/>
+            </div>
 
         </div>
 
