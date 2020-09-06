d1388d172598a2e1e3ccfd67bcd147d536700287
Verdilac <64456879+Verdilac@users.noreply.github.com>
2020-09-06 22:07:36 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //Using BEM for the first time
+    <div className="app">
+      <div className = "app__body">
+        <Sidebar /> 
+
+        {/* chat */}
+
+      </div>
     </div>
   );
 }
 
 export default App;
+   
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgHo-nBZRD43kRJeWFYBxbJ8xxAhih4dQ",
+    authDomain: "what-app-clone-31908.firebaseapp.com",
+    databaseURL: "https://what-app-clone-31908.firebaseio.com",
+    projectId: "what-app-clone-31908",
+    storageBucket: "what-app-clone-31908.appspot.com",
+    messagingSenderId: "246621729158",
+    appId: "1:246621729158:web:120770fce9c58187773c07",
+    measurementId: "G-QNPQQKDB6C"
+  }; 
